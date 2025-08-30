ed5f45f15cf4afef909374fb592ec680b4e53ac3
Pixciv <snackzmind@gmail.com>
2025-08-30 15:29:27 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,8 @@
 plugins {
-    id("com.android.application") version "8.1.0" apply false
-    kotlin("android") version "1.9.0" apply false
+    id("com.android.application") version "8.4.0" apply false
+    kotlin("android") version "1.9.22" apply false
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
